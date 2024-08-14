c84d99cd656adac1d5303b38e30fafbda38100a0
dongsu <tlsehd930@gmail.com>
2024-08-14 12:02:31 +0900

    FEAT : 회원가입 시 로직 Update - 아이디 유효성 검사 API 추가 - 회원가입 시 첫 위치 추가

--- src/main/java/org/duckdns/wru/global/auth/controller/AuthController.java
+++ src/main/java/org/duckdns/wru/global/auth/controller/AuthController.java
@@ -3,8 +3,10 @@ package org.duckdns.wru.global.auth.controller;
 import lombok.RequiredArgsConstructor;
 import org.duckdns.wru.global.auth.dto.JoinRequest;
 import org.duckdns.wru.global.auth.service.AuthService;
+import org.duckdns.wru.global.common.dto.ApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,8 +18,14 @@ public class AuthController {
     private final AuthService authService;
 
     @PostMapping("/join")
-    public ResponseEntity<?> joinProcess(JoinRequest joinRequest) {
+    public ResponseEntity<?> joinProcess(@RequestBody JoinRequest joinRequest) {
         authService.joinProcess(joinRequest);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/isExist")
+    public ResponseEntity<?> isExistProcess(@RequestBody JoinRequest joinRequest) {
+        boolean isExist =  authService.isExist(joinRequest);
+        return ResponseEntity.ok(ApiResponse.success(isExist));
     }
 }
--- src/main/java/org/duckdns/wru/global/auth/service/AuthService.java
+++ src/main/java/org/duckdns/wru/global/auth/service/AuthService.java
@@ -3,10 +3,10 @@ package org.duckdns.wru.global.auth.service;
 import lombok.RequiredArgsConstructor;
 import org.duckdns.wru.app.user.domain.User;
 import org.duckdns.wru.app.user.repository.UserRepository;
+import org.duckdns.wru.app.userLocation.domain.UserLocation;
+import org.duckdns.wru.app.userLocation.repository.UserLocationRepository;
 import org.duckdns.wru.global.auth.dto.JoinRequest;
-import org.duckdns.wru.global.auth.dto.LoginResponse;
 import org.duckdns.wru.global.auth.exception.DuplicateDataException;
-import org.duckdns.wru.global.common.dto.ApiResponse;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -16,10 +16,13 @@ public class AuthService {
 
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final UserLocationRepository userLocationRepository;
 
     public void joinProcess(JoinRequest joinRequest) {
         String username = joinRequest.getUsername();
+        System.out.println("username = " + username);
         String password = joinRequest.getPassword();
+        System.out.println("password = " + password);
 
         Boolean isExist = userRepository.existsByUsername(username);
 
@@ -30,6 +33,18 @@ public class AuthService {
         User user = new User(username, bCryptPasswordEncoder.encode(password), "ROLE_ADMIN");
         userRepository.save(user);
 
+        // 서울시청의 위치를 기본 위치로 설정
+        double defaultLatitude = 37.5665;
+        double defaultLongitude = 126.9780;
 
+        UserLocation initialLocation = new UserLocation(user, defaultLatitude, defaultLongitude);
+        userLocationRepository.save(initialLocation);
+    }
+
+    public boolean isExist(JoinRequest joinRequest) {
+        String username = joinRequest.getUsername();
+        System.out.println("username = " + username);
+        if(username == null) { return true;}
+        return userRepository.existsByUsername(username);
     }
 }
